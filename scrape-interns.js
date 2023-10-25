c8e723a7533041d331a98e8a9aecf25d787be65a
Ciprian Tomoiaga <ciprian.tomoiaga@gmail.com>
2023-10-25 16:12:29 +0200

    refactor so that we re-parse the students table to avoid lost context

--- scrape-interns.js
+++ scrape-interns.js
@@ -2,6 +2,8 @@ import puppeteer from 'puppeteer';
 import fs from 'fs';
 import path from 'path';
 
+let page; // GLOBAL variable used to interact with the browser page
+
 const destDir = process.argv[2];
 if (!destDir) {
     console.error('Usage: node scrape-interns.js /path/to/destination/folder');
@@ -20,6 +22,86 @@ async function XHR(ms) {
     return new Promise(r => setTimeout(r, ms || 2000));
 }
 
+async function XHRLoading(promise) {
+    return Promise.all([
+        promise,
+        page.waitForNavigation(),
+        page.waitForNetworkIdle(),
+    ])
+}
+
+async function getFirstVisible(node, selector, successMsg) {
+    // not working: page.waitForSelector(selector, {visible: true})
+    // see https://github.com/puppeteer/puppeteer/issues/6389
+    const matches = await node.$$(selector);
+    for (let match of matches)
+        if (await match.boundingBox()) {
+            if (successMsg)
+                console.log(successMsg);
+            return match;
+        }
+    throw new Error(`Could not find element for selector "${selector}"`);
+}
+
+async function processStudent() {
+    // there are many iframes, but the shown one is in a div that ends with `_900`
+    const studentDataIFrameSelector = '[id$=_900] > iframe';
+    const studFrameElement = await page.$(studentDataIFrameSelector);
+    if (!studFrameElement) {
+        console.error('Could not find student frame', studFrameElement);
+        process.exit(1);
+    }
+
+    const studFrame = await studFrameElement.contentFrame();
+    await page.waitForNetworkIdle();
+
+    const studNameElement = await studFrame.$('td.inscrstage-entr-infos-etu');
+    const studName = await studNameElement.evaluate(node => node.innerText);
+    console.log('On page of student: %s', studName);
+
+    const studDir = path.join(destDir, studName);
+    if (!fs.existsSync(studDir)) {
+        fs.mkdirSync(studDir);
+    }
+
+    const studEmail = await studFrame.$eval('td.inscrstage-entr-infos-email'     , el => el.innerText);
+    const studPhone = await studFrame.$eval('td.inscrstage-entr-infos-tel'       , el => el.innerText);
+    const studInternship = await studFrame.$eval('td.inscrstage-entr-infos-stage', el => el.innerText);
+
+    console.log(studEmail, studPhone, studInternship);
+
+    const filesElements = await studFrame.$$('table.prtl-se-affichage a');
+    console.log('Found %d files', filesElements.length);
+
+    for (let file of filesElements) {
+        const fileName = await file.evaluate(node => node.innerText);
+        const filePath = path.join(studDir, fileName);
+        if (fs.existsSync(filePath)) {
+            console.log('File %s exists', fileName);
+            continue;
+        }
+
+        // make browser download files to a directory
+        // see: https://github.com/puppeteer/puppeteer/issues/299#issuecomment-1767664921
+        const client = await page.target().createCDPSession();
+        await client.send('Page.setDownloadBehavior', {
+            behavior: 'allow',
+            downloadPath: studDir,
+        });
+
+        await file.evaluate((a, fn) => {
+            a.removeAttribute('target');
+            a.setAttribute('download', fn);
+            console.log('setting download to', fn);
+        }, fileName);
+        console.log(fileName);
+
+        await file.click();
+    }
+
+    const returnButtonPromise = studFrame.$('div.inscrstage-entr-retour a');
+    return returnButtonPromise;
+}
 
 (async () => {
     // start Google Chrome from "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --remote-debugging-port=21222
@@ -44,18 +126,19 @@ async function XHR(ms) {
         console.error("You don't have a 2nd tab open, and we don't want to overwrite your 1st tab.")
         process.exit(1);
     }
-    const page = pages[1];
+
+    // Setup global page variable
+    page = pages[1];
     await page.setViewport({width: 1365, height: 1330}); // recommended to set some size
     await page.setDefaultTimeout(5000);
-
-    // needed to download files
-    const client = await page.target().createCDPSession();
 
     // When you're on mac and using proxy, this page takes AGES to load
     // most probably due to the proxy. The network request is pending, rather than stuck at the server
     // So, I run this through a tunnel to bypass the proxy
     // Update Oct 2023: it seems to work through the proxy now, no worries
-    await page.goto('https://isa.epfl.ch/imoniteur_ISAP/isacademia.htm');
+    await XHRLoading(
+        page.goto('https://isa.epfl.ch/imoniteur_ISAP/isacademia.htm'),
+    );
     try {
         // throw an error if we're not logged in
         const loginSelector = '#ww_x_username';
@@ -79,140 +162,56 @@ async function XHR(ms) {
 
     const frame = page.frames().find(frame => frame.name() === "principal");
     // go to the "Gestion des stages (portail entreprise)"
-    await Promise.all([
+    await XHRLoading(
         frame.click('td > a'),
-        page.waitForNavigation(),
-    ]);
+    );
+    console.log('Navigated to "Gestion des stages"');
 
-    // click on the number of registered students, to see them all
+    // get the internship table and click on the number of registered students, to see them all
     const internshipTablesSelector = 'table[name*=listeStage]';
-    const internshipTables = await page.$$(internshipTablesSelector);
-    console.log('Found %d internship tables, filtering for visible one...', internshipTables.length)
-    let internshipTable = null;
-    for (let table of internshipTables) {
-        if (await table.boxModel()) {
-            internshipTable = table;
-            break;
-        }
-    }
-    if (!internshipTable) {
-        console.error('Could not find internship table');
-        process.exit(1);
-    }
-    else {
-        console.log('Found !')
-    }
-
+    // NOTE: in my tests, this "wait for visible" does not work reliably. previous method was better
+    const internshipTable = await getFirstVisible(page, internshipTablesSelector,
+        "Found internship table");
     console.log('Going to registered students...')
 
     const registeredStudentsSelector = 'td:nth-of-type(5) > a'
     const registeredStudents = await internshipTable.$(registeredStudentsSelector);
-    await Promise.all([
+    await XHRLoading(
         registeredStudents.click(),
-        page.waitForNavigation(),
-    ]);
-    await XHR(); // after navigation, not at the same time
+    );
 
-    const tables = await page.$$('table.prtl-se-Table.prtl-se-affichageListPostulants');
-    console.log('Found %d tables with students. Filtering for visible ones', tables.length);
-
-    const visibleTables = [];
-    for (let table of tables) {
-        const bm = await table.boxModel();
-        // console.log(bm);
-        if (bm)
-            visibleTables.push(table);
-    }
-    console.log('Filtered down to %d visible tables of registered students', visibleTables.length);
-    if (visibleTables.length !== 1) {
-        console.error('Wrong... exiting');
-        process.exit(1);
-    }
-    const table = visibleTables[0];
-
+    // Unfortunately, the portal loads student pages dynamically via XHR requests
+    // This means that when we access a student, we lose current context.
+    // As such, we cannot iterate through the nice `students` table, as the elements are detached when we get back
+    // => we iterate through integers, and re-select the table each time we come back to the page
+    const registeredStudentsTablesSelector = 'table.prtl-se-Table.prtl-se-affichageListPostulants';
     const studentsSelector = 'td:first-of-type';
-    const students = await table.$$(studentsSelector);
-    console.log('Found %d students', students.length);
+    let students = [];
+    let currStudentIdx = 0;
+    do {
+        // const studentsTable = await page.waitForSelector(registeredStudentsTablesSelector, {visible: true});
+        const studentsTable = await getFirstVisible(page, registeredStudentsTablesSelector);
 
+        // first column has student name
+        students = await studentsTable.$$(studentsSelector);
+        const student = students[currStudentIdx];
 
-    for (let student of students) {
-        // go to student
-        await Promise.all([
-            page.waitForNavigation(),
-            student.click(),
-        ])
-        await XHR(); // after navigation, not at the same time
+        // enter student
+        await XHRLoading(
+            student.click()
+        );
+        const backToStudentsButton = await processStudent();
 
+        // exit student
+        await XHRLoading(
+            backToStudentsButton.click(),
+        );
 
-        // [ABANDONED]
-        // Here I put a debugger to understand why the below did not work
-        // it turns out that there is a frame in the page, so that I need to
-        // search for the element in that frame, not in the parent page
-        // at this point, I had run out of time and had to do it manually
-        // await page.evaluate(() => { debugger; });
+        currStudentIdx++;
+    } while (currStudentIdx < students.length);
 
-        // this is the iframe selector[id$=_900] > iframe
-        // because there are many iframes, but the shown one is in a div that
-        // ends with `_900`
-        const studFrameElement = await page.$('[id$=_900] > iframe');
-        let studFrame;
-        if (!studFrameElement) {
-            console.error('Could not find student frame', studFrameElement);
-            process.exit(1);
-        }
-        else {
-            console.log('Found student frame ! 🤞');
-            studFrame = await studFrameElement.contentFrame();
-        }
-        await XHR();
+    console.log('DONE 🎉');
 
-        const studNameElement = await studFrame.$('td.inscrstage-entr-infos-etu');
-        const studName = await studNameElement.evaluate(node => node.innerText);
-        console.log('On page of student: %s', studName);
-        const studDir = path.join(destDir, studName);
-        if (!fs.existsSync(studDir)) {
-            fs.mkdirSync(studDir);
-        }
-
-        const studEmail = await studFrame.$eval('td.inscrstage-entr-infos-email'     , el => el.innerText);
-        const studPhone = await studFrame.$eval('td.inscrstage-entr-infos-tel'       , el => el.innerText);
-        const studInternship = await studFrame.$eval('td.inscrstage-entr-infos-stage', el => el.innerText);
-
-        console.log(studEmail, studPhone, studInternship);
-
-        const filesElements = await studFrame.$$('table.prtl-se-affichage a');
-        console.log('Found %d files', filesElements.length);
-
-        for (let file of filesElements) {
-            const fileName = await file.evaluate(node => node.innerText);
-
-            const client = await page.target().createCDPSession();
-            await client.send('Page.setDownloadBehavior', {
-                behavior: 'allow',
-                downloadPath: studDir,
-            });
-
-            await file.evaluate((a, fn) => {
-                a.removeAttribute('target');
-                a.setAttribute('download', fn);
-                console.log('setting download to', fn);
-            }, fileName);
-            console.log(fileName);
-
-            await file.click();
-        }
-
-        // return to list of students
-        const returnButton = await studFrame.$('div.inscrstage-entr-retour a');
-        await Promise.all([
-            returnButton.click(),
-            page.waitForNavigation(),
-        ])
-        await XHR();
-    }
-
-
-
-    // NO NEED to close the browser, it's the one we started on command line
-    // await browser.close();
+    // without this, the script doesn't finish
+    await browser.close();
 })()
